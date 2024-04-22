0eac2809ff37a53e0496afa6699933f6bd0eee59
valeriogonini <valeriogonini@gmail.com>
2024-04-22 16:14:31 +0200

    add nav bar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,7 +1,33 @@
 <template>
-    <div>
+    <header>
+        <div class="container-lg">
+            <div class="row header-1">
+                <ul class="lista-1">
+                    <li>
+                        <a href=""><img class="img-1" src="/public/img/HelloIMG1669149314369.png" alt=""></a>
+                    </li>
 
-    </div>
+                </ul>
+                <ul class="lista-1">
+                    <li>
+                        <a href="">Home</a>
+                    </li>
+                    <li>
+                        <a href="">Mission</a>
+                    </li>
+                    <li>
+                        <a href="">Causes</a>
+                    </li>
+                    <li>
+                        <a href="">Journal</a>
+                    </li>
+                    <li>
+                        <button class="button-inizia"><a href="">Donate</a></button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 </template>
 
 <script>
